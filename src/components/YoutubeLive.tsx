c725f066daeda0cb749976e7075074b04610bcb3
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-05-11 20:38:04 +0600

    test: fb, yt live

--- src/components/YoutubeLive.tsx
+++ src/components/YoutubeLive.tsx
@@ -11,6 +11,17 @@ const YouTubeLive = () => {
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
             ></iframe>
+
+            <iframe
+                src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FJamunaTelevision%2Fvideos%2F1857262375067653%2F&show_text=false&width=560&t=0"
+                width="560"
+                height="314"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameBorder="0"
+                allowFullScreen={true}
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+            ></iframe>
         </div>
     );
 };
